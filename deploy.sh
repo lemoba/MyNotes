b549633bc3d104f032a3d9240aa4378b314bb140
ranen <lemoba@163.com>
2022-05-12 10:32:59 +0800

    add Dockerfile

--- deploy.sh
+++ deploy.sh
@@ -6,17 +6,19 @@ set -e
 # 生成静态文件
 npm run docs:build
 
-# 进入生成的文件夹
-cd docs/.vuepress/dist
+# 将githubaction 和 Dockerfile移动过来
+cd docs/.vuepress/docker
+cp -R . ../dist
 
-# 如果是发布到自定义域名
-# echo 'www.example.com' > CNAME
+# 进入生成的文件夹
+cd ../dist
+
 
 git init
 git add -A
 git commit -m 'deploy'
 
-# 如果发布到 https://<USERNAME>.github.io
+# # 如果发布到 https://<USERNAME>.github.io
 git push -f git@github.com:lemoba/lemoba.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
